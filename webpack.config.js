c2f1268a1c3ce68696bceb83d683ef43df22e7ea
Hristo Deshev <hristo@deshev.com>
2016-08-02 18:28:58 +0300

    Webpack now works with livesync (no --watch yet!)

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 var bundler = require("nativescript-dev-webpack");
+var ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
 var path = require("path");
 
 var config = bundler.getConfig({
 });
 
 config.context = path.resolve("./app");
-config.resolve.extensions.push(".ts");
+config.resolve.extensions.unshift(".ts");
 config.entry.bundle = "./main.ts";
-config.output.path = path.resolve("./app");
+//config.output.path = path.resolve("./app");
+
 config.module = {
     loaders: [
         {
@@ -16,6 +18,8 @@ config.module = {
         }
     ]
 };
+
+config.plugins.push(new ForkCheckerPlugin());
 
 console.log(JSON.stringify(config, null, "    "));
 module.exports = config;
